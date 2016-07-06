1058bdce875493c2c14a0af066f26d15200cca35
Nicholas Swift <nicholas.w.swift@gmail.com>
2016-07-05 19:38:55 -0700

    updates
    Removed tableview, added scrollview.

--- Overwatch Guide/HeroInfoViewController.swift
+++ Overwatch Guide/HeroInfoViewController.swift
@@ -9,47 +9,18 @@
 import Foundation
 import UIKit
 
-class HeroInfoViewController: UITableViewController {
-    
-    // Main Stuff
+class HeroInfoViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // Table View Stuff
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        switch indexPath.row {
-        case 0:
-            let cell = tableView.dequeueReusableCellWithIdentifier("BannerCell") as! BannerTableViewCell
-            cell.didLoad()
-            return cell
-        case 1:
-            let cell = tableView.dequeueReusableCellWithIdentifier("CountersCell")
-            //tableView.reloadData()
-            return cell!
-        case 2:
-            let cell = tableView.dequeueReusableCellWithIdentifier("InfoCell")
-            //tableView.reloadData()
-            return cell!
-        default:
-            let cell = tableView.dequeueReusableCellWithIdentifier("BannerCell")
-            //tableView.reloadData()
-            return cell!
+        //Set up scroll view to fit perfectly
+        /*var contentRect: CGRect = CGRectZero
+        for view: UIView in scrollView.subviews {
+            contentRect = CGRectUnion(contentRect, view.frame)
         }
-        
+        scrollView.contentSize = contentRect.size*/
     }
     
 }
