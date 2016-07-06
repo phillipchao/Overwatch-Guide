6b2eb07f5ab70e97ce513fc05378378d837f865e
Nicholas Swift <nicholas.w.swift@gmail.com>
2016-07-06 12:54:19 -0700

    updates
    updates

---
+++ Overwatch Guide/JSONHelper.swift
@@ -0,0 +1,15 @@
+//
+//  JSONHelper.swift
+//  Overwatch Guide
+//
+//  Created by Nicholas Swift on 7/6/16.
+//  Copyright © 2016 Phillip Chao. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class JSONHelper {
+    
+}
